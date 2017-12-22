8f8d32a1871b6ba3e48e5e9fef98716146c827f1
Jeff Saul <Jeff.Saul@sandstone.com.au>
2017-12-22 17:46:46 +1100

    Renamed test class.

--- src/test/java/au/com/sandstone/FacebookOAuth/FacebookGitHubOAuth20ApplicationTests.java
+++ src/test/java/au/com/sandstone/FacebookOAuth/FacebookGitHubOAuth20ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FacebookOAuthApplicationTests {
+public class FacebookGitHubOAuth20ApplicationTests {
 
 	@Test
 	public void contextLoads() {
